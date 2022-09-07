39a5d0347c1d0735f6134eeda2a20f0e3adb730e
AdithyaPadmashali <adithya.p2001@gmail.com>
2022-09-07 14:07:53 +0530

    ran commit-sub

--- test.tf
+++ test.tf
@@ -1,26 +1,8 @@
 
 
-resource "cloudflare_record" "test" {
+resource "cloudflare_record" "testing" {
   zone_id    = var.zone_id
-  name       = "test"
-  value      = aws_eip.eip.public_dns
-  type       = "CNAME"
-  ttl        = 1
-  proxied    = true
-  depends_on = [aws_instance.public]
-}
-resource "cloudflare_record" "first" {
-  zone_id    = var.zone_id
-  name       = "first"
-  value      = aws_eip.eip.public_dns
-  type       = "CNAME"
-  ttl        = 1
-  proxied    = true
-  depends_on = [aws_instance.public]
-}
-resource "cloudflare_record" "first" {
-  zone_id    = var.zone_id
-  name       = "first"
+  name       = "testing"
   value      = aws_eip.eip.public_dns
   type       = "CNAME"
   ttl        = 1
